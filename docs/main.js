fca58bd7a55b95da73416d759dca085394c2230e
nasa14119 <nico.amaya0205@gmail.com>
2022-08-22 21:08:20 -0500

    Flags don't exploate bombs

--- docs/main.js
+++ docs/main.js
@@ -1,12 +1,14 @@
 // Variables Globales
-var columna = 6; 
-var fila = 6; 
-var total_botones = 0;
+let columna = 6; 
+let fila = 6; 
+let total_botones = 0;
+const imgbomba = '<i class="fas fa-bomb"></i>'
+const imgbandera = '<i class="fas fa-flag" aria-hidden="true"></i>'; 
 const board = []; 
-var bombas = 8;
-var bombasrestantes;  
-var banderas_puestas = 0; 
-var bombasocultas = [] 
+let bombas = 8;
+let bombasrestantes;  
+let banderas_puestas = 0; 
+let bombasocultas = [] 
 // Codigo central
 window.addEventListener('DOMContentLoaded', main)
 function main(){
@@ -19,13 +21,12 @@ function display_bombas(params) {
     let label = document.getElementById('message_bomba'); 
     label.innerHTML = params; 
 }
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
 function addBombs(params) {
     bombas = params;
-    // message('¨Cantidad de bombas = '+bombas)
     for (let i = 0; i < bombas; i++) {
-        function getRandomNumber(min, max) {
-            return Math.floor(Math.random() * (max - min)) + min;
-        }
         let randomNumber = getRandomNumber(1,total_botones);
         let Lugardebomba_id = board[randomNumber];
         let lugardelabomba = document.getElementById(Lugardebomba_id)
@@ -37,48 +38,49 @@ function addBombs(params) {
     display_bombasrestantes(); 
 } 
 function bombexploation(event) {
-    let imgbomba = '<i class="fas fa-bomb"></i>'
     let actualbtn = document.getElementById(event.target.id)
-    message('Perdiste')
-    for (let i = 0; i < bombasocultas.length; i++) {
-        let b = bombasocultas[i]
-        let x = document.getElementById('button-'+b)
-        x.innerHTML = imgbomba; 
-    }
-    for (let i = 1; i < board.length; i++) {
-        let x = document.getElementById('button-'+i)
-        x.removeEventListener('click', addflag); 
-    }
-    actualbtn.innerHTML = imgbomba; 
-}
-function addflag(event) {
-    let bandera = '<i class="fas fa-flag"></i>'; 
-    let boton_actual = document.getElementById(event.target.id);
-    if (boton_actual.innerHTML == "&nbsp;") 
-    { 
-        boton_actual.innerHTML = bandera; 
-        banderas_puestas++; 
-        bombasrestantes--; 
-        display_bombasrestantes();
-        console.log (banderas_puestas + bombasrestantes) 
-    } 
-        else {boton_actual.innerHTML = '&nbsp;';banderas_puestas--;bombasrestantes++;display_bombasrestantes();console.log (banderas_puestas + bombasrestantes) } 
-    if (bombasrestantes == banderas_puestas){
+    if (actualbtn.innerHTML !== imgbandera){
+        console.log(actualbtn.innerHTML)
+        actualbtn.innerHTML = imgbomba; 
+        for (const i in bombasocultas) {
+            let b = bombasocultas[i]
+            let x = document.getElementById('button-'+b)
+            x.innerHTML = imgbomba; 
+        }
         for (let i = 1; i < board.length; i++) {
             let x = document.getElementById('button-'+i)
             x.removeEventListener('click', addflag); 
         }
+        message('Perdiste')
+    }
+}
+function addflag(event) {
+    event.preventDefault(); // Para que no salga el contex menu 
+    let boton_actual = document.getElementById(event.target.id);
+    if (boton_actual.innerHTML == "&nbsp;") 
+    { 
+        boton_actual.innerHTML = imgbandera; 
+        banderas_puestas++; 
+        bombasrestantes--; 
+        display_bombasrestantes();
+    } 
+        else {boton_actual.innerHTML = '&nbsp;';banderas_puestas--;bombasrestantes++;display_bombasrestantes(); } 
+    if (bombasrestantes == banderas_puestas){
+        for (let i = 1; i < board.length; i++) {
+            let x = document.getElementById('button-'+i)
+            x.removeEventListener('contexmenu', addflag); 
+        }
     } else if (bombasrestantes > banderas_puestas){
         for (let i = 1; i < board.length; i++) {
             let x = document.getElementById('button-'+i)
-            x.addEventListener('click', addflag); 
+            x.addEventListener('contexmenu', addflag); 
         }
     }
 }
 function Flag_logic() {
     for (let id = 1; id < total_botones; id++) {
         let actualbtn = document.getElementById("button-" + id);
-        actualbtn.addEventListener('click', addflag);
+        actualbtn.addEventListener('contextmenu', addflag);
         actualbtn.innerHTML = "&nbsp;";
         board[id] = actualbtn.id; 
     }
